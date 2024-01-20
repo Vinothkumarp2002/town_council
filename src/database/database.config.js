b555901912d37a0efc11a77662a9b0cc23cb1a17
Vinothkumarp2002 <vinothkumarp@students.tcarts.in>
2024-01-20 22:42:57 +0530

    commit 4

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -4,10 +4,10 @@ const dotenv =require('dotenv')
 dotenv.config();
 
 const config = {
-    server: '97.74.83.13',
-    database: 'dev.shatechno',
-    user: 'dev.shatechno', 
-    password:'7!jYo787n' ,
+    server: "97.74.83.13",
+    database: "dev.shatechno",
+    user: "dev.shatechno", 
+    password:"7!jYo787n",
     options: {
         trustedConnection: false,
         enableArithAbort: true,
