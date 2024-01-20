4db895a6456ce449072944a156e6a1bc3d87903c
Vinothkumarp2002 <79747444+Vinothkumarp2002@users.noreply.github.com>
2024-01-20 15:25:53 +0530

    Update WorkerOrder.routes.js

--- src/WorkerOrderTransaction/WorkerOrder.routes.js
+++ src/WorkerOrderTransaction/WorkerOrder.routes.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const { createWorkOrder, createImageTransaction, getAllWorkOrder, getAllWorkOrderTransaction } = require("./WorkerOrder.controllers");
+const { createWorkOrder, createImageTransaction, getAllWorkOrder, getAllWorkOrderTransaction,getWorkOrderTransaction } = require("./WorkerOrder.controllers");
 
 const router =express.Router()
 
 router.post('/workOrder',createWorkOrder);
 
-router.get('/workOrder',getAllWorkOrder);
+router.get('/workOrder',getWorkOrderTransaction);
 
 router.get('/workOrderTransaction',getAllWorkOrderTransaction);
 
 router.post('/image',createImageTransaction);
+
+routet.get('/image',getAllWorkOrder);
+
+
 
 
 module.exports =router
